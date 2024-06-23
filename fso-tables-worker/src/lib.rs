50c8466aea673ebc445707650acfacbcad591dde
John Fernandez <jafernandez.tampabay@gmail.com>
2024-06-23 11:01:53 -0400

    More progress

--- fso-tables-worker/src/lib.rs
+++ fso-tables-worker/src/lib.rs
@@ -1,14 +1,18 @@
 use axum::
     {
-        {
+        http::StatusCode,
         routing::{get, post, patch, delete},
         Router,
-        },
-    http::StatusCode
     };
 use tower_service::Service;
 use worker::*;
 use serde::{Deserialize, Serialize};
+
+const USER_LEVEL_OWNER: u8 = 0;
+const USER_LEVEL_ADMIN: u8 = 1; // Able to upgrade other users to a maintainer
+const USER_LEVEL_MAINTAINER: u8 = 2; // Able to make changes to table docs
+const USER_LEVEL_VIEWER: u8 = 3; 
+const DB_NAME: str = "fso-modoption-d1-db";
 
 /*#[derive(Serialize, Deserialize)]
 struct PasswordResetRequest {
@@ -20,23 +24,39 @@ struct PasswordResetRequest {
 //  POST, GET, PATCH, and DELETE -- PUTS AND PATCHES are going to be the same.
 
 fn router() -> Router {
+    let db = ctx.env.d1(DB_NAME)?;
+
     Router::new()
     .route("/", get(root))
-    .route("/users", get(user_stats_get))       // No Post, get, put, patch, or delete for overarching category
+    .route("/users", get(user_stats_get))       // No Post, put, patch, or delete for overarching category
     .route("/users/:username", 
         post(user_register_new).get(user_get_details).put(api_insufficent_permissions).patch(api_insufficent_permissions).delete(deactivate_user))
+    .route("/users/:username/passwordchange", put(user_change_password).patch(user_change_password).delete(api_insufficent_permissions))    // No post, get or delete for password
+    .route("/users/:username/upgrade", put(upgrade_user_permissions).patch(upgrade_user_permissions))
+    .route("/users/:username/downgrade", put(downgrade_user_permissions).patch(downgrade_user_permissions))
+    .route("/users/:username/email", post(add_email).put(add_email).patch(add_email).delete(api_insufficent_permissions))
+    .route("/users/:username/activateemail/:code", post(confirm_email_address))
     .fallback(api_fallback)
 }
 
+/* 
+user_register_new
+user_get_details
+deactivate_user
+user_change_password
+upgrade_user_permissions
+downgrade_user_permissions
+add_email
+confirm_email_address
+*/
+
 /*
+    .route("/tables", get(table_stats_get).post(table_post).delete(api_insufficent_permissions))
+    .route("/tables", )
+    .route("/tables", get(table_stats_get))
+    .route("/tables", get(table_stats_get))
     .route("/tables", get(table_stats_get))
     .route("/tables/:tid", get(table_get_details))
-    .route("/tables", post(table_post))
-    .route("/tables", patch(table_stats_get))
-    .route("/tables", get(table_stats_get))
-    .route("/tables", get(table_stats_get))
-    .route("/tables", get(table_stats_get))
-    .route("/tables/", get(user))
  */
 /*
     .route("/deprecations/", get(user))
@@ -87,10 +107,10 @@ pub async fn api_fallback() -> (StatusCode, &'static str) {
 use worker::*;
 
 #[derive(Deserialize)]
-struct Thing {
-	thing_id: String,
-	desc: String,
-	num: u32,
+struct Registration {
+	email: String,
+	password: String,
+
 }
 
 #[event(fetch, respond_with_errors)]
