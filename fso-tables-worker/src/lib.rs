7f805fc1617adb790b763972c406e3324d0e5a92
John Fernandez <jafernandez.tampabay@gmail.com>
2024-07-06 11:38:35 -0400

    Some more router progress

--- fso-tables-worker/src/lib.rs
+++ fso-tables-worker/src/lib.rs
@@ -6,7 +6,7 @@ use email_address::*;
 const USER_LEVEL_OWNER: u8 = 0;
 const USER_LEVEL_ADMIN: u8 = 1; // Able to upgrade other users to a maintainer
 const USER_LEVEL_MAINTAINER: u8 = 2; // Able to make changes to table docs
-const USER_LEVEL_VIEWER: u8 = 3;  */
+const USER_LEVEL_VIEWER: u8 = 3; // Just here until someone upgrades to a maintainer level  */
 const DB_NAME: &str = "fso_table_database";
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -22,8 +22,9 @@ async fn fetch(req: Request, env: Env, _ctx: Context,) -> Result<Response> {
     Router::new()
         .get_async("/", root_get)
         .get_async("/users", user_stats_get)       // No Post, put, patch, or delete for overarching category
-    /*    .route("/users/:username", 
-            post(user_register_new).get(user_get_details).put(api_insufficent_permissions).patch(api_insufficent_permissions).delete(deactivate_user))
+        .post_async("/users/register", user_register_new)
+        .get_async("/users/:username", user_get_details)\
+        .put_async(api_insufficent_permissions).patch(api_insufficent_permissions).delete(deactivate_user))
         .route("/users/:username/passwordchange", put(user_change_password).patch(user_change_password).delete(api_insufficent_permissions))    // No post, get or delete for password
         .route("/users/:username/upgrade", put(upgrade_user_permissions).patch(upgrade_user_permissions))
         .route("/users/:username/downgrade", put(downgrade_user_permissions).patch(downgrade_user_permissions))
@@ -78,35 +79,44 @@ pub async fn user_stats_get(_: Request, _ctx: RouteContext<()>) -> worker::Resul
 }
 
 pub async fn user_register_new(_: Request, _ctx: RouteContext<()>) -> worker::Result<Response> {  
+    // FINISH ME!
+
     err_api_under_construction()
 }
 
 pub async fn user_get_details(_: Request, _ctx: RouteContext<()>) -> worker::Result<Response> {  
     // FINISH ME!
+    err_api_under_construction()
 }
 
 pub async fn deactivate_user(_: Request, _ctx: RouteContext<()>) -> worker::Result<Response> {  
     // FINISH ME!
+    err_api_under_construction()
 }
 
 pub async fn user_change_password(_: Request, _ctx: RouteContext<()>) -> worker::Result<Response> {  
     // FINISH ME!
+    err_api_under_construction()
 }
 
 pub async fn user_upgrade_user_permissions(_: Request, _ctx: RouteContext<()>) -> worker::Result<Response> {  
     // FINISH ME!
+    err_api_under_construction()
 }
 
 pub async fn user_downgrade_user_permissions(_: Request, _ctx: RouteContext<()>) -> worker::Result<Response> {  
     // FINISH ME!
+    err_api_under_construction()
 }
 
 pub async fn user_add_email(_: Request, _ctx: RouteContext<()>) -> worker::Result<Response> {  
     // FINISH ME!
+    err_api_under_construction()
 }
 
 pub async fn user_confirm_email_address(_: Request, _ctx: RouteContext<()>) -> worker::Result<Response> {  
     // FINISH ME!
+    err_api_under_construction()
 }
 
 /*
@@ -127,8 +137,6 @@ pub async fn user_confirm_email_address(_: Request, _ctx: RouteContext<()>) -> w
 //pub async fn table_get_details(params(":tid")) -> &'static str {
 //    "FINISH ME! = Table get details"
 //}
-
-pub async
 
 // Failures
 pub async fn err_insufficent_permissions() -> worker::Result<Response> {
@@ -159,23 +167,5 @@ use worker::*;
 struct Registration {
 	email: String,
 	password: String,
-
 }
 
-#[event(fetch, respond_with_errors)]
-pub async fn main(request: Request, env: Env, _ctx: Context) -> Result<Response> {
-	Router::new()
-		.get_async("/:id", |_, ctx| async move {
-			let id = ctx.param("id").unwrap()?;
-			let d1 = ctx.env.d1("things-db")?;
-			let statement = d1.prepare("SELECT * FROM things WHERE thing_id = ?1");
-			let query = statement.bind(&[id])?;
-			let result = query.first::<Thing>(None).await?;
-			match result {
-				Some(thing) => Response::from_json(&thing),
-				None => Response::error("Not found", 404),
-			}
-		})
-		.run(request, env)
-		.await
-} */
