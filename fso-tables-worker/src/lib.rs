e3c7941f797b496a151740f5867ad4ad3029b90e
JohnAFernandez <jafernandez.tampabay@gmail.com>
2024-07-07 00:05:49 -0400

    Write a basic registration API route
    Still need to originate a confirmation email.
    
    Our responses don't have the correct format, either.  Status codes are not status codes...

--- fso-tables-worker/src/lib.rs
+++ fso-tables-worker/src/lib.rs
@@ -9,7 +9,7 @@ const USER_LEVEL_MAINTAINER: u8 = 2; // Able to make changes to table docs
 const USER_LEVEL_VIEWER: u8 = 3; // Just here until someone upgrades to a maintainer level  */
 const DB_NAME: &str = "fso_table_database";
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Deserialize, Serialize)]
 struct GenericResponse {
     status: u16,
     message: String,
@@ -29,7 +29,7 @@ async fn fetch(req: Request, env: Env, _ctx: Context,) -> Result<Response> {
         .get_async("/", root_get)
         .get_async("/users", user_stats_get)       // No Post, put, patch, or delete for overarching category
         .post_async("/users/register", user_register_new)
-        .get_async("/users/:username", user_get_details)/*
+        .get_async("/users/:username", user_get_details)/* 
         .put_async(api_insufficent_permissions).patch(api_insufficent_permissions).delete(deactivate_user))
         .route("/users/:username/passwordchange", put(user_change_password).patch(user_change_password).delete(api_insufficent_permissions))    // No post, get or delete for password
         .route("/users/:username/upgrade", put(upgrade_user_permissions).patch(upgrade_user_permissions))
@@ -58,56 +58,136 @@ pub async fn user_stats_get(_: Request, _ctx: RouteContext<()>) -> worker::Resul
                 Ok(r) => {
                     match r {
                         Some(r2) => Response::from_json(&r2) ,
-                        None => return err_specific_error("Internal server erorr, query returned no count".to_string()).await,
+                        None => return err_specific("Internal server erorr, query returned no count".to_string()).await,
                     }
                 },
-                Err(e) => return err_specific_error(e.to_string()).await,
+                Err(e) => return err_specific(e.to_string()).await,
             }
         }
-        Err(e) => return err_specific_error(e.to_string()).await,
+        Err(e) => return err_specific(e.to_string()).await,
     }            
 }
 
-pub async fn user_register_new(_: Request, ctx: RouteContext<()>) -> worker::Result<Response> {  
-    // FINISH ME!
-//    ctx.
-
-    err_api_under_construction().await
+#[derive(Serialize, Deserialize)]
+struct EmailSubmission{
+    email: String,
 }
 
-pub async fn user_get_details(_: Request, _ctx: RouteContext<()>) -> worker::Result<Response> {  
-    // FINISH ME!
-    err_api_under_construction().await
+pub async fn user_register_new(mut req: Request, ctx: RouteContext<()>) -> worker::Result<Response> {  
+    let submission = req.json::<EmailSubmission>().await;
+    if submission.is_err() {
+        return err_bad_request().await;
+    }
+    
+    let email = submission.unwrap();
+
+    if !EmailAddress::is_valid(&email.email){
+        return Response::from_json(&GenericResponse {
+            status: 200,
+            message: "Email address is not in the right format".to_string(),
+        })
+    }
+
+    let db = ctx.env.d1(DB_NAME);
+    match &db{
+        Ok(db1) => {
+            if db_does_user_exists(&email.email, &db1).await {
+                return Response::from_json(&GenericResponse {
+                    status: 200,
+                    message: "Email address is already taken.".to_string(),
+                })
+            };
+            
+            let statement = db1.prepare("INSERT INTO users (username, role, active) VALUES (?, 3, 0)").bind(&[email.email.into()]);
+            match &statement {
+                Ok(q) => {
+                    if let Err(e) = q.run().await {
+                        return err_specific(e.to_string()).await;
+                    }
+
+                    // TODO! need to send a confirmation email here
+
+                    return Response::from_json(&GenericResponse {
+                        status: 200,
+                        message: "User created!".to_string(),
+                    })    
+                },
+                Err(e) => return err_specific(e.to_string()).await,
+            }
+        },
+        Err(e) => return err_specific(e.to_string()).await,
+    }
+
 }
 
-pub async fn deactivate_user(_: Request, _ctx: RouteContext<()>) -> worker::Result<Response> {  
-    // FINISH ME!
-    err_api_under_construction().await
+pub async fn user_get_details(_: Request, ctx: RouteContext<()>) -> worker::Result<Response> {  
+    let db = ctx.env.d1(DB_NAME);
+    match &db{
+        Ok(_) => {
+            return err_api_under_construction().await            
+        },
+        Err(e) => return err_specific(e.to_string()).await,
+    }
 }
 
-pub async fn user_change_password(_: Request, _ctx: RouteContext<()>) -> worker::Result<Response> {  
-    // FINISH ME!
-    err_api_under_construction().await
+pub async fn deactivate_user(_: Request, ctx: RouteContext<()>) -> worker::Result<Response> {  
+    let db = ctx.env.d1(DB_NAME);
+    match &db{
+        Ok(_) => {
+            return err_api_under_construction().await            
+        },
+        Err(e) => return err_specific(e.to_string()).await,
+    }
 }
 
-pub async fn user_upgrade_user_permissions(_: Request, _ctx: RouteContext<()>) -> worker::Result<Response> {  
-    // FINISH ME!
-    err_api_under_construction().await
+pub async fn user_change_password(_: Request, ctx: RouteContext<()>) -> worker::Result<Response> {  
+    let db = ctx.env.d1(DB_NAME);
+    match &db{
+        Ok(_) => {
+            return err_api_under_construction().await            
+        },
+        Err(e) => return err_specific(e.to_string()).await,
+    }
 }
 
-pub async fn user_downgrade_user_permissions(_: Request, _ctx: RouteContext<()>) -> worker::Result<Response> {  
-    // FINISH ME!
-    err_api_under_construction().await
+pub async fn user_upgrade_user_permissions(_: Request, ctx: RouteContext<()>) -> worker::Result<Response> {  
+    let db = ctx.env.d1(DB_NAME);
+    match &db{
+        Ok(_) => {
+            return err_api_under_construction().await            
+        },
+        Err(e) => return err_specific(e.to_string()).await,
+    }
 }
 
-pub async fn user_add_email(_: Request, _ctx: RouteContext<()>) -> worker::Result<Response> {  
-    // FINISH ME!
-    err_api_under_construction().await
+pub async fn user_downgrade_user_permissions(_: Request, ctx: RouteContext<()>) -> worker::Result<Response> {  
+    let db = ctx.env.d1(DB_NAME);
+    match &db{
+        Ok(_) => {
+            return err_api_under_construction().await            
+        },
+        Err(e) => return err_specific(e.to_string()).await,
+    }
 }
 
-pub async fn user_confirm_email_address(_: Request, _ctx: RouteContext<()>) -> worker::Result<Response> {  
-    // FINISH ME!
-    err_api_under_construction().await
+pub async fn user_add_email(_: Request, ctx: RouteContext<()>) -> worker::Result<Response> {  
+    let db = ctx.env.d1(DB_NAME);
+    match &db{
+        Ok(_) => {
+            return err_api_under_construction().await            
+        },
+        Err(e) => return err_specific(e.to_string()).await,
+    }
+}
+
+pub async fn user_confirm_email_address(_: Request, ctx: RouteContext<()>) -> worker::Result<Response> {  
+    let db = ctx.env.d1(DB_NAME);
+    match &db{
+        Ok(_) => {
+            return err_api_under_construction().await            
+        },
+        Err(e) => return err_specific(e.to_string()).await,
+    }
 }
 
 /*
@@ -129,7 +209,27 @@ pub async fn user_confirm_email_address(_: Request, _ctx: RouteContext<()>) -> w
 //    "FINISH ME! = Table get details"
 //}
 
-// Failures
+#[derive(Serialize, Deserialize)]
+struct Enabled{
+    active: i32,
+}
+
+// SECTION!! generic database tasks 
+pub async fn db_does_user_exists(email: &String, db: &D1Database) -> bool {
+    let query = db.prepare("SELECT active FROM users WHERE username = ?").bind(&[email.into()]).unwrap();
+
+    match query.first::<Enabled>(None).await {
+        Ok(r) => {
+            match r {
+                Some(_) => return true,
+                None => return false,
+            }
+        },
+        Err(e) => panic!("{}", e.to_string()),
+    }
+}
+
+// SECTION!! Body/Server Failure Responses
 pub async fn err_insufficent_permissions() -> worker::Result<Response> {
     Response::from_json(&GenericResponse {
         status: 403,
@@ -151,7 +251,14 @@ pub async fn err_api_under_construction() -> worker::Result<Response> {
     })
 }
 
-pub async fn err_specific_error(e: String) -> worker::Result<Response> {
+pub async fn err_bad_request() -> worker::Result<Response> {
+    Response::from_json(&GenericResponse {
+        status: 400,
+        message: "Bad request, check your json input".to_string(),
+    })
+}
+
+pub async fn err_specific(e: String) -> worker::Result<Response> {
     Response::from_json(&GenericResponse {
         status: 500,
         message: e
