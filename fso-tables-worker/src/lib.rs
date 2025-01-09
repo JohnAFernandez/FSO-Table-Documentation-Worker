7cefa50bd1466d501a92567887eabafa0af838cc
John Fernandez <jafernandez.tampabay@gmail.com>
2025-01-09 00:25:00 -0500

    Hopefully finish formatting these emails

--- fso-tables-worker/src/lib.rs
+++ fso-tables-worker/src/lib.rs
@@ -50,7 +50,29 @@ impl EmailMessage {
             sender : FullEmailAddress::create_full_email("FSO Tables Database User Activations".to_string(), "activations@fsotables.com".to_string()),
             to : vec![], 
             subject : "Account Confirmation Code".to_string(),
-            htmlContent : format!("<div style=\"background-color: #000000; padding-top: 20px; padding-bottom: 20px; border-radius: 5px;\"><div style=\"text-align:center; font-family: calibri; padding: 50px; background-color: #242424; color: #cecece; margin-left:50px; margin-right:50px; border-radius: 5px;\"><h2>Welcome to Ganymede, the Freespace Open Table Option Database</h2><h3>To activate your account enter this confirmation code at the ganymede website</h3><div style=\"display:flex; justify-content: center;\"><h1 style=\"background-color: #333333; border-radius: 5px; padding:10px; padding-left:15px; padding-right:15px; letter-spacing: 0.5em;\">{}</h1></div><br><h4>Account confirmation codes expire in 24 hours.<br><br>If you are unsure why you got this email, please permanently it.<br><br>Please do not reply, this email address is not monitored.</h4></div></div>", code),
+            htmlContent : format!("
+            <div style=\"background-color: #000000; padding-top: 20px; padding-bottom: 20px; border-radius: 5px;\">
+                <div style=\"text-align:center; font-family: calibri; padding: 50px; background-color: #242424; color: #cecece; margin-left:50px; margin-right:50px; border-radius: 5px;\">
+                    <h2>
+                        Welcome to Ganymede, the Freespace Open Table Option Database
+                    </h2>
+                    <h3>
+                        To activate your account enter this confirmation code at the ganymede website
+                    </h3>
+                    <div style=\"display: grid;\">
+                        <h1 style=\"background-color: #333333; border-radius: 5px; padding:10px; padding-left:15px; padding-right:15px; letter-spacing: 0.5em;\">
+                            {}
+                        </h1>
+                    </div>
+                    <br>
+                    <h4>Account confirmation codes expire in 24 hours.
+                        <br><br>
+                        If you are unsure why you got this email, please permanently delete it.
+                        <br><br>
+                        Please do not reply, this email address is not monitored.
+                    </h4>
+                </div>
+            </div>", code),
         }
     }
 
@@ -59,7 +81,29 @@ impl EmailMessage {
             sender : FullEmailAddress::create_full_email("FSO Tables Database Password Reset".to_string(), "credentials@fsotables.com".to_string()),
             to : vec![], 
             subject : "Account Reset Code".to_string(),
-            htmlContent : format!("<div style=\"background-color: #000000; padding-top: 20px; padding-bottom: 20px; border-radius: 5px;\"><div style=\"text-align:center; font-family: calibri; padding: 50px; background-color: #242424; color: #cecece; margin-left:50px; margin-right:50px; border-radius: 5px;\"><h2>We received a password reset request for your Ganymede account.<br>Here is your confirmation code:</h2><div style=\"display:flex; justify-content: center;\"><h1 style=\"background-color: #333333; border-radius: 5px; padding:10px; padding-left:15px; padding-right:15px; letter-spacing: 0.5em;\">{}</h1></div><br><h4>Confirmation codes expire in 30 minutes.<br><br>If you are unsure why you got this code, please permanently delete this email.<br><br>Please do not reply, this email address is not monitored.</h4></div></div>", code),
+            htmlContent : format!("
+            <div style=\"background-color: #000000; padding-top: 20px; padding-bottom: 20px; border-radius: 5px;\">
+                <div style=\"text-align:center; font-family: calibri; padding: 50px; background-color: #242424; color: #cecece; margin-left:50px; margin-right:50px; border-radius: 5px;\">
+                    <h2>
+                        We received a password reset request for your Ganymede account.
+                        <br>
+                        Here is your confirmation code:
+                    </h2>
+                    <div style=\"display: grid;\">
+                        <h1 style=\"background-color: #333333; border-radius: 5px; padding:10px; padding-left:15px; padding-right:15px; letter-spacing: 0.5em;\">
+                            {}
+                        </h1>
+                    </div>
+                    <br>
+                    <h4>
+                        Confirmation codes expire in 30 minutes.
+                        <br><br>
+                        If you are unsure why you got this code, please permanently delete this email.
+                        <br><br>
+                        Please do not reply, this email address is not monitored.
+                    </h4>
+                </div>
+            </div>", code),
         }
     }
 }
